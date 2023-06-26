5f6c471b6074e1bce445dc52c1e8ece16452ac2a
Saagar <saagar@semprehealth.com>
2023-06-26 00:24:28 -0400

    Filling out more info

--- arisaagar/src/App.js
+++ arisaagar/src/App.js
@@ -35,7 +35,7 @@ function App() {
           <div className="bgBox" style={{'backgroundImage':"url(https://i.imgur.com/NtcMidB.jpg)"}}>
           </div>               
           <h1 id="coverpage_0" className="logo">
-            <img src="https://i.imgur.com/xdGLMnQ.png"></img>
+            <img src="https://i.imgur.com/zP9PuEm.png"></img>
           </h1>
         </section>
 
@@ -175,9 +175,16 @@ function App() {
                   There are so many things to do in Borcay, including but not limited to: island hopping, water sports, snorkeling and diving, nightlife, hiking, and cliff diving. 
                   <br />
                   <br />
-                  <a href="https://guidetothephilippines.ph/articles/what-to-experience/boracay-tourist-spots" target="_blank" rel="nofollow">Click here to see more</a>.
+                  <a href="https://guidetothephilippines.ph/articles/what-to-experience/boracay-tourist-spots" target="_blank" rel="nofollow"><u>Click here to see more</u></a>.
                   <br />
-                  In November, it's gonna be warm. [TODO add more details]. Crimson Resort is nice too.
+                  Back in the day, boats used to be allowed to bring tourists directly to Boracay's White Beach. They would drop off tourists at boat staion 1
+                  in the north to boat station 3 in the south part of White Beach. The Boat stations have been replaced by the singular port, but the naming of Boracay's
+                  stations have remained. Today, Station 1 in the north of White Beach is home to luxorious and peaceful beach resorts. Station 2 is the epicenter of the
+                  island, home to the biggest shopping areas and night life. Station 3 is home to the backpacker hostels and nature.
+                  <br />
+                  <br />
+                  <a href="https://www.crimsonhotel.com/boracay/about-us" target="_blank" rel="nofollow" style={{'display':"inline"}}><u>The Crimson Resort and Spa in Boracay</u></a> is not on White Beach, and is located in the coveted Station 0 of Boracay in the very north part of the island.
+                  This is the most tranquil and isolated part of the island, and home to many 5-star resorts. The Crimson hosts a secluded and pristine beach, a pool bar, and four superb restaurants.
                 </h4>
               </div>
             </div>
@@ -197,13 +204,62 @@ function App() {
             </div>
           </div>
 
-          <div className="section stagger_l ">             
+          <div className="section stagger_r ">             
             <div className="sectionCont large">
               <div id="head_11" className="textCont textOnly">
                 <h2>WEATHER</h2>
                 <div className="subsection">
                   <h4>
-                    It's gonna be hot yo.
+                    The dry season on Boracay starts in November. The average daytime temperatures are around 30°C (86°F) and nighttime temperatures are around 23°C (74°F). 
+                    It is of course highly receommended to wear reef safe sunscreen and to stay hydrated while out under the sun.
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="section stagger_l ">             
+            <div className="sectionCont large">
+              <div id="head_11" className="textCont textOnly">
+                <h2>GETTING THERE</h2>
+                <div className="subsection">
+                  <h3>Flight Booking</h3>
+                  <h4>
+                    For international travelers, it is recommended to book a roundtrip flight to Manila, and then arrange domestic travel afterwards.
+                    <br />
+                    <br />
+                    From Manila, it is recommended to fly into Caticlan (Airport Code: MPH). We suggest that you book your flights ahead of time. Below are the airlines that fly from Manila to Caticlan daily.
+                    <br />
+                    <br />
+                    <b>Philippine Airlines</b> - <a href="https://www.philippineairlines.com/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.philippineairlines.com/</a>
+                    <br />
+                    <b>Cebu Pacific Air</b> - <a href="https://www.cebupacificair.com/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.cebupacificair.com/</a>
+                    <br />
+                    <b>AirAsia</b> - <a href="https://www.airasia.com/en/gb" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.airasia.com/en/gb</a>
+                    <br />
+                    <b>Royal Air PH</b> - <a href="https://flyroyalair.com/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://flyroyalair.com/</a>
+                    <br />
+                    <br />
+                    From Caticlan airport, it is a short vehicle ride to Jetty port, and a 10-20 minute ferry ride to the island.
+                    <br />
+                    <br />
+                    If you are staying at the Crimson, please forward your confirmed flight itinerary to Crimson Reservation so they can 
+                    arrange your pick-up from Caticlan Airport to the resort.
+                  </h4>
+                </div>
+                  <div className="subsection">
+                  <h3>Airport Transport Booking</h3>
+                  <h4>
+                    If your accommodation does not include airport transfers and you wish to avail, here are some of the Transport Company in the Island that offers shared door to door transfer and private transfer via Caticlan Airport:
+                    <br />
+                    <br />
+                    <b>Layod’s Travel & Tours</b> - <a href="https://www.facebook.com/layodstransport/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.facebook.com/layodstransport/</a>
+                    <br />
+                    <b>Southwest Travel & Tours</b> - <a href="https://www.southwesttours.com.ph/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.southwesttours.com.ph/</a>
+                    <br />
+                    <b>MyBoracayGuide</b> - <a href="https://www.myboracayguide.com/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.myboracayguide.com/</a>
+                    <br />
+                    <b>Island Star Express</b> - <a href="https://www.islandstarexpress.com/" target="_blank" rel="nofollow" style={{'display':"inline"}}>https://www.islandstarexpress.com/</a>
                   </h4>
                 </div>
               </div>
@@ -215,9 +271,49 @@ function App() {
               <div id="head_11" className="textCont textOnly">
                 <h2>WHERE TO STAY</h2>
                 <div className="subsection">
+                  <h4>During the time of the wedding, it is recommended to stay in station 0 or 1, so as to have quick and easy access to the venue. 
+                  Should you arrive before the events, or decide to stay after, it is recommended to stay in station 1 or 2 so as to be more central
+                  on the island and have acess to the island's amentieis and activities.</h4>
+                  < br/>
                   <h3>STATION 0</h3>
                   <h4>
-                    Close to Crimson yo
+                    Station 0 has many five star resorts and private villas for rent on AirBnb.
+                    <br />
+                    <br />
+                    The Crimson Resort and Spa has provided the wedding code <b>WEDDING15</b> (15% discount based on Best Available Online Rates) for guests of the wedding.
+                     To reserve a room and take advantage of special rates, booking must be booked thru <a href="https://booking.crimsonhotel.com/ibe/search?hid=4145&lang=EN&cur=PHP&_affclk=adn:3817:2159795:EAIaIQobChMI1sW5g73T_wIVlNTICh3BKAfBEAAYAiAAEgInmPD_BwE:8002y1:#q&guests=A,A&in=2023-11-28&out=2023-12-1&coupon=WEDDING15" target="_blank" rel="nofollow" style={{'display':"inline"}}><u>the Crimson Boracay Website</u></a>
+                    <br/>
+                    <br/>
+                    <b>RATE INCLUSIONS:</b>
+                    <br />
+                    <i>Daily breakfast for two (2) persons</i>
+                    <br />
+                    <br />
+                    <i>Complimentary Round-trip land and boat transfers from Godofredo Ramos Domestic Airport or Caticlan Jetty port for rooms booked with an average room rate above P11,000++ per night. Prior notice of confirmed flight or arrival details are required to arrange transfers.</i>
+                    <br />
+                    <br />
+                    <i>Complimentary mini-bar items upon arrival</i>
+                    <br />
+                    <br />
+                    <i>One-time welcome healthy drink to be served in the available outlet</i>
+                    <br />
+                    <br />
+                    <i>Access to the Fitness Centre, swimming pools and beach</i>
+                    <br />
+                    <br />
+                    <i>Unlimited Hi-speed WIFI internet access</i>
+                    <br />
+                    <br />
+                    You may also contact them through their email and contact numbers below:
+                    <br />
+                    <br />
+                    <b>T</b>: (+36) 669 5888 
+                    <br />
+                    <b>T Local</b>: (+36) 286 2700 
+                    <br />
+                    <b>M</b>: (+63) 998 596 4626 
+                    <br />
+                    <b>E</b>: boracay.reservation@crimsonhotel.com 
                   </h4>
                 </div>
               </div>
